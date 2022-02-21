92ccd9d2330ee309c59045a86462e1121e82541a
joaodoamaral13 <59985844+joaodoamaral13@users.noreply.github.com>
2022-02-21 23:21:26 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -4,13 +4,28 @@ from observador import getArticle as getArticle
 
 app = Flask(__name__)
 
-@app.route("/{url}", methods = ["GET"])
-def getArt(url):
+@app.route("/", methods = ["GET"])
+def home():
+
+    return "FreeObservador"
+
+@app.route("/getarticle", methods = ["GET"])
+def getarticle():
 
 
     if request.method == "GET":
 
-        return getArticle(url)
+        url = request.args.get("url")
+        
+        if url != "":
+            article = getArticle(url)
+
+            if article == "":
+                article = "Não foi possivel buscar o artigo"
+        else:
+            article = "Não foi possivel buscar o artigo"
+
+        return article 
 
 
 
